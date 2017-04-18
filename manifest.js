c6bf6f5d72a6044e370daa439134d21c87d5569a
Kam Low <auscaster@gmail.com>
2017-04-18 14:33:47 +0200

    Update manifest

--- manifest.js
+++ manifest.js
@@ -1,11 +1,10 @@
 var fs = require('fs')
-  , path = require('path')
-  , exec = require('child_process').exec;
+  , path = require('path');
 
 function combineManifest(callback) {
   fs.readdir(__dirname, function(err, filenames) {
     if (err) {
-      onError(err);
+      callback(err);
       return;
     }
     filenames = filenames.filter(function (filename) {
@@ -16,74 +15,30 @@ function combineManifest(callback) {
       manifest.push(require(path.join(__dirname, filename)));
     });
 
-    console.log('Combined manifest', manifest)
-    fs.writeFile('manifest.json', JSON.stringify(manifest), 'utf8', callback);
+    manifest.sort(function(a,b) {
+      if (parseInt(a.revision) > parseInt(b.revision))
+        return -1;
+      if (parseInt(a.revision) < parseInt(b.revision))
+        return 1;
+      return 0;
+    });
+
+    if (manifest.length) {
+      var latest = manifest[0].revision;
+      manifest.forEach(function(build) {
+        if (build.revision == latest)
+          build.latest = true;
+      });
+    }
+
+    fs.writeFile('manifest.json', JSON.stringify(manifest, null, 2), 'utf8', callback);
   });
 }
 
 // Combine the manifest files
-combineManifest(function() {
-
-  // Push to git
-  console.log('Pushing')
-  // exec('git add -A', function(err, stdout, stderr) {
-  //   if (err != null) {
-  //     console.log('Error', err)
-  //     throw new Error(err);
-  //   } else if (typeof(stderr) != "string") {
-  //     console.log('Error', stderr)
-  //     throw new Error(stderr);
-  //   } else {
-  //     console.log(stdout)
-  //   }
-
-  // exec('git commit -m "Update build"', function(err, stdout, stderr) {
-  //   if (err != null) {
-  //     console.log('Error', err)
-  //     throw new Error(err);
-  //   } else if (typeof(stderr) != "string") {
-  //     console.log('Error', stderr)
-  //     throw new Error(stderr);
-  //   } else {
-  //     console.log(stdout)
-  //   }
-
-  // exec('git push origin master', function(err, stdout, stderr) {
-  //   if (err != null) {
-  //     console.log('Error', err)
-  //     throw new Error(err);
-  //   } else if (typeof(stderr) != "string") {
-  //     console.log('Error', stderr)
-  //     throw new Error(stderr);
-  //   } else {
-  //     console.log(stdout)
-  //   }
-  // });
-  // });
-
-  // });
-
-  // exec('git push origin master', function(err, stdout, stderr) {
-  //   if (err != null) {
-  //     console.log('Error', err)
-  //     throw new Error(err);
-  //   } else if (typeof(stderr) != "string") {
-  //     console.log('Error', stderr)
-  //     throw new Error(stderr);
-  //   } else {
-  //     console.log(stdout)
-  //   }
-  // });
-  
-  // exec('git add -A && git commit -m "Update build" && git push origin master', function(err, stdout, stderr) {
-  //   if (err != null) {
-  //     console.log('Error', err)
-  //     throw new Error(err);
-  //   } else if (typeof(stderr) != "string") {
-  //     console.log('Error', stderr)
-  //     throw new Error(stderr);
-  //   } else {
-  //     console.log(stdout)
-  //   }
-  // });
+combineManifest(function(err) {
+  if (err)
+    console.log('Error', err)
+  else
+    console.log('Combined manifest')
 });
